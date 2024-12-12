a29ea265fb4c5cf2e64fed33038d72c0c23ee586
Muhammad Usman <usmanazhar357@gmail.com>
2024-12-12 15:45:10 +0500

    commit on 12-12-2024

---
+++ database/migrations/2024_12_12_102616_create_companies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unique();
+            $table->string('phone', 20)->nullable();
+            $table->string('resume', 200)->nullable();
+            $table->string('linkedin_profile', 255)->nullable();
+            $table->string('portfolio', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
